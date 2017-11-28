c289e938769d984af8060bc52058b26637c2467e
Millorad <training2@nsi.it>
2017-11-28 15:57:53 +0100

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,9 +17,10 @@ app.post('/todos',(req, res) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
-})
+});
 });
 
 app.listen(3000, () => {
     console.log('Started on port 3000')
 });
+ module.exports = {app};
