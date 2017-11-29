f7bdf4c0bb9908f275e0490e99d6bf092b116052
Millorad <training2@nsi.it>
2017-11-29 16:18:54 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} =require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} =require('mongodb');
 
-var {mongoose} = require ('./db/mongoose');
-var {Todo} = require('./models/todo');
+var mongoose = require ('./db/mongoose');
+var Todo = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
@@ -11,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req, res) => {
+app.post('/todos',         (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
@@ -59,13 +60,40 @@ app.delete('/todos/:id',(req,res)=> {
         if(!todo) {
             return res.status(404).send();
 }
-        res.send(todo);
+        res.send({todo});
     }).catch((e)=> {
         res.status(400).send();
 })
 })
 
+app.patch('/todos/:id',(req,res)=> {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+            if(!todo){
+                return res.status(404).send();
+            }
+
+            res.send(todo);
+        }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
 app.listen(port, () => {
     console.log('Started up at port '+port);
 });
- module.exports = {app};
+ module.exports = app;
