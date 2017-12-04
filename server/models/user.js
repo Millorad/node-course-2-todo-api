3707fbf0888efca2446227ea78d70df2709aa8e8
Millorad <training2@nsi.it>
2017-12-04 10:42:21 +0100

    Add POST users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -59,7 +59,7 @@ UserSchema.methods.generateAuthToken = function () {
     return user.save().then(() => {
         return token;
     });
-}
+};
 
 UserSchema.statics.findByToken = function (token) {
     var User = this;
@@ -76,7 +76,27 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.token': token,
         'tokens.access': 'auth'
     });
-}
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                }else {
+                    reject();
+                }
+            });
+        });
+    });
+};
 
 UserSchema.pre('save', function (next) {
     var user = this;
@@ -91,8 +111,8 @@ UserSchema.pre('save', function (next) {
     }else {
         next();
     }
-})
+});
 
-var User = mongoose.model('User', UserSchema)
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
